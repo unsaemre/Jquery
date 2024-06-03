f0a8a6ca3d07ec24f3b4832e947d9754dabab5d8
jun <gmdwns9516@gmail.com>
2024-06-03 10:34:51 +0900

    1

---
+++ basic/js/06.js
@@ -0,0 +1,7 @@
+// 요소의 넓이와 높이
+// width(), height()
+// innerwidth , innerHeight 여백을 포함한 넓이와 높이
+// outerwidth , outerHeight 여백 및 선을 포함한 넓이와 높이
+
+console.log($('.p1').width());
+console.log($('.p1').innerWidth());
